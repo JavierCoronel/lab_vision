aeefc2dc1004b0bf704e30ca95e9352b7687e452
JavierCoronel <jd.coronel30@uniandes.edu.co>
2016-02-17 20:46:05 -0500

    Create find_duplicates.sh

---
+++ lab01_git/find_duplicates.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Run in image directory
+find -not -empty -type f -printf "%s\n" | sort -rn | uniq -d | xargs -I{} -n1 find -type f -size {}c -print0 | xargs -0 md5sum | sort | uniq -w32 --all-repeated=separate
+
+#Source commandlinefu.com/commands/by/noahspurrier
