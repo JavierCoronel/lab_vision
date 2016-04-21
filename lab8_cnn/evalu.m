7f8cc93e1982d95d53e10a50ce1ed46b49e56521
JavierCoronel <jd.coronel30@uniandes.edu.co>
2016-04-21 09:39:01 -0500

    Added files via upload

--- lab8_cnn/evalu.m
+++ lab8_cnn/evalu.m
@@ -1,3 +1,4 @@
+setup ;
 imdb = load('textonsdb.mat') ;
 imdb.images.data = im2single(imdb.images.data) ;
 train_index = find(mod(0:length(imdb.images.id)-1,2) < 1) ;
@@ -6,16 +7,9 @@ imdb.images.set(1,val_index)=2;
 
 net=load('textures_jitter.mat');
 
-
-results_train = train_net(net,imdb.images.data(:,:,train_index)) ;
-results_train_anot = imdb.images.label(train_index) ;
-
-results_test = train_net(net,imdb.images.data(:,:,val_index)) ;
-results_test_anot = imdb.images.label(val_index) ;
+results_train = train_net(net,imdb.images.data(:,:,1:2));
+results_train_anot = imdb.images.label(1:2) ;
 
 save('results_train_anot');
 save('results_train');
-save('results_test_anot');
-save('results_test');
-
 
--- lab8_cnn/train_net.m
+++ lab8_cnn/train_net.m
@@ -1,4 +1,4 @@
-function resultados=train_net(net,test_data)
+function res=train_net(net,test_data)
 
 
   test_data = im2single(test_data) ;
@@ -12,13 +12,13 @@ function resultados=train_net(net,test_data)
   for i=1:size(test_data,3)
 
   resultados = vl_simplenn(net, test_data(:,:,i)) ;
-  scores = squeeze(gather(resultados(end).x)) ;
+  scores = squeeze(gather(resultados(end).x(1,1,:))) ;
+  [val,pos]=max(scores);
   %Obtain the best label based on the highest score
-  scores=scores(:);
-  [bestS(i),best(i)] = max(scores) ;
+  res(i)=pos;
   clc;
   end
 
-  
+%   resultados=best;
 
 end
